71c091dfc093262c202c1b3b9f4dd1c12b9880c5
Quarsatos <manuelpapula69@gmail.com>
2022-09-29 12:48:54 +0200

    basic

---
+++ Papula/basic-exercise/javascirpt/main.js
@@ -0,0 +1,8 @@
+let sandwiches = JSON.parse(sandwiches)
+let fries = JSON.parse(fries)
+
+for  (let sandwich of sandwiches){
+    for (let frie of fries){
+        document.getElementById("demo").innerHTML +=
+    }
+}
